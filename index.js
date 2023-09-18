8897301317b7707c22c362e372ae120c6c83b4f0
Jeongho Park <d0lbangi13@naver.com>
2023-09-19 01:07:50 +0900

    Add registration function to check if requests is responded

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
+
+const { User } = require("./models/User");
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://jeonghopark:thfla1823612@cluster0.sp9hwu8.mongodb.net/?retryWrites=true&w=majority', {
-  // useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-}).then(() => console.log('MongoDB Connected....'))
+mongoose.connect('mongodb+srv://jeonghopark:thfla1823612@cluster0.sp9hwu8.mongodb.net/?retryWrites=true&w=majority', {})
+  .then(() => console.log('MongoDB Connected....'))
   .catch(err => console.log(err))
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.post('/register', async (req, res) => {
+  // req.body로 클라이언트에서 보낸 데이터를 받아와 User 모델을 생성
+  const user = new User(req.body);
+
+  try {
+    // 데이터베이스에 저장
+    const userInfo = await user.save();
+    res.status(200).json({ success: true });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      err: err.message, // 에러 메시지를 응답으로 전달
+    });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- models/User.js
+++ models/User.js
@@ -7,8 +7,8 @@ const userSchema = mongoose.Schema({
   },
   email: {
     type: String,
-    trim: true, // 스페이스의 공백을 없애줌
-    unique: 1 // 똑같은 email을 쓰게 하지 못하도록
+    trim: true,
+    unique: 1
   },
   password: {
     type: String,
@@ -19,18 +19,19 @@ const userSchema = mongoose.Schema({
     maxlength: 50
   },
   role: {
-    type: Number, // ex) number:0 관리자, number: 일반유저
-    default: 0 // 임의로 role을 정하지 않으면 role의 type 을 0으로 주겠다.
+    type: Number,
+    default: 0
   },
   image: String,
-  token: { // 유효성
+  token: {
     type: String
   },
   tokenExp: {
     type: Number
   }
-})
+});
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model('User', userSchema);
 
-mongoose.model.export = {User}
+// module.exports로 변경합니다.
+module.exports = { User };
